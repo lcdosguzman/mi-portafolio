536dca38afe61aee2e742a59a4270f51644b4eb4
Simon Guzman <lcdosguzman@gmail.com>
2024-01-30 19:26:25 -0300

    migraciones

--- ProyectoPortafolio/AppPortafolio/models.py
+++ ProyectoPortafolio/AppPortafolio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Tecnologia(models.Model):
+    nombre = models.CharField(max_length=80)
+    descripcion = models.TextField()
+    experiencia = models.IntegerField()
